1d5e28902eff8d65c468f094ecd417b2c761840e
Wonders Victor <wondersprince1@gmail.com>
2023-10-02 11:52:28 +0000

    Corrected 0-9

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-printi(f"{number} Battery street")
+printi(f"{number:d} Battery street")
--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
 import this
-
-zen = this.s.splitlines()[:28]
-for line in zen:
-    print(line)
