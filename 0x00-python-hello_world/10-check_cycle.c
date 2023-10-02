d503d1fdcf50ed25c4db2adbaed7a81f11252187
Wonders Victor <wondersprince1@gmail.com>
2023-10-02 13:40:07 +0000

    Modified 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,17 +10,17 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *tortoise = list;
-	listint_t *hare = list->next;
+	listint_t *hare = list;
 
 	if (!list)
 		return (0);
 
 	while (tortoise && hare && hare->next)
 	{
-		if (tortoise == hare)
-			return (1);
 		tortoise = tortoise->next;
 		hare = hare->next->next;
+		if (tortoise == hare)
+			return (1);
 	}
 	return (0);
 }
