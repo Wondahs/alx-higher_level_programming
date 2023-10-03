231fea6956c70e83f5630678b5dfcbef47c65aa6
Wonders Victor <wondersprince1@gmail.com>
2023-10-03 11:22:07 +0000

    Corrected 3 and 4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')):
+for letter in range(ord('a'), ord('z') + 1):
     if not (chr(letter) == 'q' or chr(letter) == 'e'):
         print("{}".format(chr(letter)), end='')
