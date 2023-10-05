16442ef7dfcd893f1296ccad852736d817a0727a
Wonders Victor <wondersprince1@gmail.com>
2023-10-05 15:03:58 +0000

    Advanced ii

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-def magic_calculation_102(a, b):
-  if a < b:
-    c = add(a, b)
-    for i in range(4, 6):
-      c = add(c, i)
-    return c
-  else:
-    return sub(a, b)
+def magic_calculation(a, b):
 
-def add(a, b):
-  return a + b
+    """Match bytecode provided by Holberton School."""
 
-def sub(a, b):
-  return a - b
-
-if __name__ == '__main__':
-  a = int(input('Enter a: '))
-  b = int(input('Enter b: '))
-  result = magic_calculation_102(a, b)
-  print(result)
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return(sub(a, b))
