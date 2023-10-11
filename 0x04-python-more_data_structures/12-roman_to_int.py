273c264a495c99f1f2f1e22f9c0a58e38b2fd2eb
Wonders Victor <wondersprince1@gmail.com>
2023-10-11 16:10:38 +0000

    Edge case in 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return (0)
     r_numerals = {
             "I": 1,
             "V": 5,
