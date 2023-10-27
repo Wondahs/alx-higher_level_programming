51b907d5baa43ee46c65fdd3467f5504f66a73e3
Wonders Victor <wondersprince1@gmail.com>
2023-10-27 07:52:45 +0000

    Working

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A module for addition"""
+
+
+def add_integer(a, b=98):
+    """
+    Function that adds a and b
+
+    >>> add_integer(1, 3)
+    4
+    >>> add_integer(2)
+    100
+
+    """
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+
+    return a + b
