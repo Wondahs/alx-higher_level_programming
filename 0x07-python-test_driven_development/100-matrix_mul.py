c679a2dfec11a3e552444ca9608595722866389c
Wonders Victor <wondersprince1@gmail.com>
2023-10-28 13:35:36 +0000

    testing

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -18,22 +18,22 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
-    if not all(isinstance(element, (int, float)) for element in row for row in m_a):
+    if not all(isinstance(element, (int, float)) for row in m_a for element in row):
         raise TypeError("m_a should contain only integers or floats")
-    if not all(isinstance(element, (int, float)) for element in row for row in m_b):
+    if not all(isinstance(element, (int, float)) for row in m_b for element in row):
         raise TypeError("m_b should contain only integers or floats")
-    if not all(len(row) == len(m_a[0] for row in m_a)):
+    if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
-    if not all(len(row) == len(m_a[0] for row in m_b)):
+    if not all(len(row) == len(m_a[0]) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
-    if len(m_a) != len(m_b[0]):
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    result = [[0 for _ in range(len(matrix2[0]))] for _ in range(len(matrix1))]
+    result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
 
-    for i in range(len(matrix1)):
-        for j in range(len(matrix2[0])):
-            for k in range(len(matrix2)):
-                result[i][j] += matrix1[i][k] * matrix2[k][j]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
 
     return result
