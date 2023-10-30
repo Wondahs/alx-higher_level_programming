59f513576acde687f0e5b5b50f1c44cbee8e3722
Wonders Victor <wondersprince1@gmail.com>
2023-10-30 13:57:58 +0000

    0-2

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module that defines a rectangle."""
+
+
+class Rectangle:
+    """Class defining rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialises rectangle"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Sets and retrives width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Sets and retrives height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
