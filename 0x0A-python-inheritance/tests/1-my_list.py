11113e9d4764c05a18fadc5576b0733128c3c3a6
Wonders Victor <wondersprince1@gmail.com>
2023-11-06 20:30:30 +0100

    tests

--- 0x0A-python-inheritance/tests/1-my_list.py
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -5,9 +5,6 @@
 class MyList(list):
     """A class that inherits from lists"""
 
-    def __init__(self, *args):
-        super().__init__(*args)
-
     def print_sorted(self):
         """Prints sorted list"""
 
