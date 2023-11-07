0135ef5cb0a33cf75b95fb522ff3b87a42f27395
Wonders Victor <wondersprince1@gmail.com>
2023-11-07 17:55:25 +0000

    workinf

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module containing function that creates an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that creates an Object from a “JSON file”
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
