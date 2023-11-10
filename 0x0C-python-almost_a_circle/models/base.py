240dcb55f2102e290e38616d23538ed43df5024c
Wonders Victor <wondersprince1@gmail.com>
2023-11-10 10:56:35 +0000

    16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Module containing base class"""
+import json
 
 
 class Base:
     """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """Instantiates Base object"""
         if id is not None:
@@ -13,14 +15,22 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Returns the JSON string representation of list_dictionaries.
         list_dictionaries is a list of dictionaries.
         """
-        import json
-
-
         if list_dictionaries in [[], None]:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+        """
+        filename = f"{cls.__name__}.json"
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(Base.to_json_string(list_objs))
