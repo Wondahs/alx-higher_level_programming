1354e34a0b8a3eac2d5f31d0ed55c632f257f169
Wonders Victor <wondersprince1@gmail.com>
2023-11-12 05:42:17 +0100

    Turtle

---
+++ 0x0C-python-almost_a_circle/turtle_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.11
+import turtle
+
+# Create a turtle screen and a turtle
+screen = turtle.Screen()
+my_turtle = turtle.Turtle()
+
+# Draw a square
+for _ in range(4):
+    my_turtle.forward(100)
+    my_turtle.left(90)
+
+# Close the turtle graphics window on click
+screen.exitonclick()
