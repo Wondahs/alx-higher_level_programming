e6a3c96c0a8094206920e7ab72c245556f144d0b
Wonders Victor <wondersprince1@gmail.com>
2023-12-13 00:16:57 +0100

    Fixed typo

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Script that lists all records
 -- of the table second_table of the database hbtn_0c_0
 SELECT score, name
-FROM hbtn_0c_0
+FROM second_table
 WHERE name != ""
 ORDER BY score DESC;
