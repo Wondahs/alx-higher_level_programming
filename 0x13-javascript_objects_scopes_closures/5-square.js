9c0dc0633d489e712d80e0d6a48fdcb16015eca8
Wonders Victor <wondersprince1@gmail.com>
2024-01-10 21:07:07 +0100

    Semistandard fixes

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Square extends require('./4-rectangle') {
-	constructor (size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 }
 
 module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 class Square extends require('./5-square.js') {
-	constructor (size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 
-	charPrint(c) {
-		if (c !== undefined) {
-			for (let i = 0; i < this.width; i++) {
-				console.log(c.repeat(this.width));
-			}
-		} else {
-			this.print();
-		}
-	}
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
 }
 
 module.exports = Square;
