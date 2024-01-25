8978f7c607765200318b4874afe64c838bcf0812
Wonders Victor <wondersprince1@gmail.com>
2024-01-25 17:13:11 +0100

    New approach for peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,17 +2,23 @@
 """contains the function find_peak"""
 
 
+# def find_peak(list_of_integers):
+#     """finds a peak in a list of unsorted integers"""
+#     list_len = len(list_of_integers)
+#     if list_len == 0:
+#         return
+#     m = list_len // 2
+#     if (
+#         (m == list_len - 1 or list_of_integers[m] >= list_of_integers[m + 1])
+#         and (m == 0 or list_of_integers[m] >= list_of_integers[m - 1])
+#     ):
+#         return list_of_integers[m]
+#     if m != list_len - 1 and list_of_integers[m + 1] > list_of_integers[m]:
+#         return find_peak(list_of_integers[m + 1:])
+#     return find_peak(list_of_integers[:m])
+
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    list_len = len(list_of_integers)
-    if list_len == 0:
-        return
-    m = list_len // 2
-    if (
-        (m == list_len - 1 or list_of_integers[m] >= list_of_integers[m + 1])
-        and (m == 0 or list_of_integers[m] >= list_of_integers[m - 1])
-    ):
-        return list_of_integers[m]
-    if m != list_len - 1 and list_of_integers[m + 1] > list_of_integers[m]:
-        return find_peak(list_of_integers[m + 1:])
-    return find_peak(list_of_integers[:m])
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
