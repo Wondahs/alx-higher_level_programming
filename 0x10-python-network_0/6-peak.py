08f40ee52d0fa030d5a65ad1ec828e61be549305
Wonders Victor <wondersprince1@gmail.com>
2024-01-25 17:02:32 +0100

    Done!

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains the function find_peak"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    list_len = len(list_of_integers)
+    if list_len == 0:
+        return
+    m = list_len // 2
+    if (
+        (m == list_len - 1 or list_of_integers[m] >= list_of_integers[m + 1])
+        and (m == 0 or list_of_integers[m] >= list_of_integers[m - 1])
+    ):
+        return list_of_integers[m]
+    if m != list_len - 1 and list_of_integers[m + 1] > list_of_integers[m]:
+        return find_peak(list_of_integers[m + 1:])
+    return find_peak(list_of_integers[:m])
