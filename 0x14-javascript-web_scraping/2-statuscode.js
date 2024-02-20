f3640e498dec180406221b9b6f79bbf4451ebb74
Wonders Victor <wondersprince1@gmail.com>
2024-02-20 23:01:02 +0100

    2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', (response) => 
+request.get(process.argv[2]).on('response', (response) =>
   console.log(`code: ${response.statusCode}`));
